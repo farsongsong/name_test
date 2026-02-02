c3af92392498fa47ab61fad7bdeb08a29bd58b91
권송현 <kksh08@naver.com>
2026-02-02 21:49:17 +0900

    Fix strokeCounts initialization and formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 획수 데이터
+
 const strokeCounts = {
     'ㄱ': 2, 'ㄲ': 4, 'ㄴ': 2, 'ㄷ': 3, 'ㄸ': 6, 'ㄹ': 5, 'ㅁ': 4, 'ㅂ': 4, 'ㅃ': 8, 'ㅅ': 2, 
     'ㅆ': 4, 'ㅇ': 1, 'ㅈ': 3, 'ㅉ': 6, 'ㅊ': 4, 'ㅋ': 3, 'ㅌ': 4, 'ㅍ': 4, 'ㅎ': 3,
@@ -101,3 +101,4 @@ function goToFinalResult() {
     document.getElementById('compare2').innerText = s2 + '%';
 
 }
+
